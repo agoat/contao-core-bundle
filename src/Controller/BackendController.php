1f3da421be89d630f5d34d32010062cade605a79
Arne Stappen <meh@agoat.de>
2016-11-23 11:08:47 +0100

    Update BackendController.php

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -31,7 +31,7 @@ use Symfony\Component\HttpFoundation\Response;
  * @author Andreas Schempp <https://github.com/aschempp>
  * @author Leo Feyer <https://github.com/leofeyer>
  *
- * @Route("/contao", defaults={"_scope" = "backend", "_token_check" = true}, condition="request.getHost() matches '/cms(.)./'", schemes="https")
+ * @Route("/contao", defaults={"_scope" = "backend", "_token_check" = true}, schemes="https")
  */
 class BackendController extends Controller
 {
